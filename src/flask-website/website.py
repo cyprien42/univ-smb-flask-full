99cdbf2be8147cdb8c6951ff69af102d7ee13e45
cyprien42 cyprien <cyprien.treboz@gmail.com>
2023-03-30 13:24:25 +0000

    tp final

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -9,4 +9,21 @@ def start():
 @app.route("/authentification")
 def auth(): 
     return render_template('authentification.html')
-    
+
+@app.route("/list")
+def liste():
+    return render_template('list_server.html')
+
+@app.route("/ajout")
+def ajout():
+    return render_template('ajout_server.html')
+
+@app.route("/suppression")
+def sup():
+    return render_template('suppression_server.html')
+
+@app.route("/list_users")
+def liste_users():
+    return render_template('list_users.html')
+
+
